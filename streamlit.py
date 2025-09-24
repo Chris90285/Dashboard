cdeb71d753520bba8db32a933ec2ab760e8d051e
Chris90285 <chris.vanpelt123@gmail.com>
2025-09-24 16:14:05 +0200

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -7,6 +7,7 @@ import plotly.graph_objects as go
 import numpy as np 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.cm as cm
 
 #-------------------data inladen-----------------------
 #-------------------------------------------------------
